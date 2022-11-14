7dd1fa7d7e1d263164e3341b0094b0f9f34e1595
Pulkit Batra <95132680+PulkitBxtra@users.noreply.github.com>
2022-11-15 04:08:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function reveal(){
 
 
 function getLocalStream() {
-  navigator.mediaDevices.getUserMedia({video: false, audio: true}).then((stream) => {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false}).then((stream) => {
       window.localStream = stream; // A
       window.localAudio.srcObject = stream; // B
       window.localAudio.autoplay = true; // C
